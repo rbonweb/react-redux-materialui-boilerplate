947668fcdabb279de66bc6be95ed99abfa9d405a
Rohit Bhadani <bhadanirohit1@gmail.com>
2021-11-02 16:23:33 +0530

    rootimport removed

--- src/components/auth/RequireAuth.jsx
+++ src/components/auth/RequireAuth.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "hooks/authContext";
+import { useAuth } from "../../hooks/authContext";
 import { PropTypes } from "prop-types";
 
 const RequireAuth = ({ component }) => {
--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,6 +1,6 @@
 import { Container, Typography } from "@mui/material";
 import React from "react";
-import Meta from "components/Meta";
+import Meta from "../components/Meta";
 
 export default function Welcome() {
   return (
